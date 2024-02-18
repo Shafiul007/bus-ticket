9da694d281362fd3560e83990fdf95cb549faaa1
Shafiul007 <shafiul1426@gmail.com>
2024-02-18 19:25:14 +0600

    apply button added for js

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ let sit=document.getElementById("seat-select");
 let seatSelect=parseInt(sit.innerText);
 console.log(seatSelect);
 console.log(seatAv);
+const apply=document.getElementById("apply");
+const coupon=document.getElementById("coupon-code");
+let sum=0;
+let totalPrice=document.getElementById("total-price");
+let grandTotal=document.getElementById("grand-total");
+let disc=document.getElementById("discount");
 for (const seat of seats){
     seat.addEventListener("click", function(){
         chooseSeat++;
@@ -28,15 +34,12 @@ for (const seat of seats){
         li.innerText=seat.innerText;
         let li1=document.createElement("li");
         let li2=document.createElement("li");
-        li1.innerText="Economic";
+        li1.innerText="Economy";
         li2.innerText=550;
         let money=parseInt(li2.innerText);
         ul1.appendChild(li);
         ul2.appendChild(li1);
-        ul3.appendChild(li2);
-        let totalPrice=document.getElementById("total-price");
-        let grandTotal=document.getElementById("grand-total");
-        let sum=0;
+        ul3.appendChild(li2); 
         for (let money of totalMoney.children){
             console.log(money.innerText);
             let amount =parseInt(money.innerText);
@@ -45,19 +48,54 @@ for (const seat of seats){
             console.log(sum);
         }
         totalPrice.innerText=sum;
-        const disc=grandTotal*0.80;
-        grandTotal.innerText=sum;
+        const disc=grandTotal*0.15;
+        let grandPrice=grandTotal.innerText.toLowerCase();
+    
        }
-       const apply=document.getElementById("apply");
+       
+       const code=coupon.value.toLowerCase();
        apply.addEventListener("click",function(){
-        if (coupon.value==="couple20" || coupon.value==="new15"){
+        if (code==="new15"){
+            grandTotal.innerText=sum*0.85;
+            disc.innerText="Discount : "+sum*0.15;
+            
 
         }
-       })
-       const coupon=document.getElementById("coupon-code");
-      
-       
+        else if(code==="couple20"){
+            grandTotal.innerText=sum*0.80;
+            disc.innerText="Discount : "+sum*0.20;
+        }
+        else{
+            grandTotal.innerText=sum;
+            disc.innerText="Discount : "+sum*0;
+            
+        }
+                
+                console.log(grandTotal.innerText);
+            })
+        // if (code==="new15"){
+        //     apply.addEventListener("click",function(){
+        //         grandTotal.innerText=sum*0.85;
+        //         console.log(grandTotal.innerText);
+        //     })
+        //     console.log("matching1")
+            
+        // }
+        // else if(code==="couple20"){
+        //     console.log("matching2");
+        //     apply.addEventListener("click",function(){
+        //         grandTotal.innerText=sum*0.80;
 
+        // })
+        // }
+        // else{
+        //     console.log("no match")
+        //     apply.addEventListener("click",function(){
+        //         grandTotal.innerText=sum;
+        //         console.log(grandTotal.innerText);
+        //     })
+        // }
+       
     })
 }
 function next(){
